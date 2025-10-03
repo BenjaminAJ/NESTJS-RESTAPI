59ff9ebffc964e130870694bde40b6df8fc90bca
Benjamin <ajanakub30@gmail.com>
2025-10-03 06:29:24 +0100

    feat(users): implement users service and integrate with controller
    -

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        { id: 1, name: 'Alice', role: 'ADMIN', email: "alice@gmail.com" },
+        { id: 2, name: 'Bob', role: 'ENGINEER', email: "bob@gmail.com"},
+        { id: 3, name: 'Charlie', role: 'INTERN', email: "charlie@gmail.com"},
+        { id: 4, name: 'David', role: 'ENGINEER', email: "dave@gmail.com"},
+        { id: 5, name: 'Eve', role: 'ADMIN', email: "eve@gmail.com"}
+    ]
+
+    findAll(role?: "INTERN" | "ENGINEER" | "ADMIN"){
+        if(role){
+            return this.users.filter(user=> user.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id: number){
+        const user = this.users.find(user => user.id === id)
+        return user
+    }
+    create(user: {name: string, email: string, role: "INTERN" | "ENGINEER" | "ADMIN"}){
+        const usersByHighestId = [...this.users].sort((a,b) => b.id - a.id);
+        const newUser = {
+            id: usersByHighestId[0].id + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+    update(id: number, updatedUser: {name?: string, email?: string, role?: "INTERN" | "ENGINEER" | "ADMIN"}){
+        this.users = this.users.map( user => {
+            if(user.id === id){
+                return {...user, ...updatedUser}
+            }
+            return user
+        })
+        return this.findOne(id)
+    }
+    delete(id:number){
+        const removedUser = this.findOne(id);
+        this.users = this.users.filter(user => user.id !== id)
+        return removedUser
+    }
+}
